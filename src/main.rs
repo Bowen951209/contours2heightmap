3ca93195ab85142a25aa205373ab92d554cd8786
bowen951209 <bowen951209@gmail.com>
2025-05-07 21:35:35 +0800

    - Contour tests and some rewrites. - Move contour_lines.png to assets.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use imageproc::image::ImageReader;
+use imageproc::contours::Contour;
+use imageproc::drawing::Canvas;
+use imageproc::image::{DynamicImage, ImageReader, Rgba};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -7,10 +9,35 @@ fn main() {
         return;
     }
     let filepath = &args[1];
-    let img = ImageReader::open(filepath)
+    let dyn_img = ImageReader::open(filepath)
         .expect("Failed to open image file")
         .decode()
         .expect("Failed to decode image");
-    let img = img.into_rgb32f();
-    imageproc::window::display_image(filepath, &img, img.width(), img.height());
+
+    let (w, h) = dyn_img.dimensions();
+    
+    let grayscale = dyn_img.to_luma8();
+    let contours: Vec<Contour<u32>> = imageproc::contours::find_contours(&grayscale);
+    println!("Contours count = {}", contours.len());
+    
+    display_contours(&contours.as_slice(), "Contours", w, h);
+    display_img(&dyn_img, filepath, w, h);
+}
+
+fn display_img(img: &DynamicImage, title: &str, w: u32, h: u32) {
+    let rgb_img = img
+        .as_rgb8()
+        .expect("Failed to convert image to RGB format");
+    imageproc::window::display_image(title, rgb_img, w, h);
+}
+
+fn display_contours(contours: &[Contour<u32>], title: &str, w: u32, h: u32) {
+    let mut img = DynamicImage::new_rgb8(w, h);
+    for contour in contours {
+        for point in &contour.points {
+            img.draw_pixel(point.x, point.y, Rgba::from([255, 0, 0, 255]));
+        }
+    }
+    
+    display_img(&img, title, w, h);
 }
