11de3b368e14a0ee328b620d8c1c5c7fa640bbf2
bowen951209 <bowen951209@gmail.com>
2025-08-29 14:31:12 +0800

    Set default RUST_LOG level to 'trace' if not configured

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+use std::env;
+
 use contours2heightmap::run;
 use log::info;
 
 fn main() {
-    pretty_env_logger::init();
-    
+    if env::var("RUST_LOG").is_err() {
+        unsafe {
+            env::set_var("RUST_LOG", "trace");
+        }
+        pretty_env_logger::init();
+        info!("RUST_LOG not set, defaulting to 'trace'"); // must be called after init
+    } else {
+        pretty_env_logger::init();
+    }
+
     run();
     info!("Program finished.");
 }
